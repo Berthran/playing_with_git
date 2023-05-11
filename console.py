0a2058876b7b87f26a7965f0db04cd9f649f605f
Daniel Berthran <70752338+Berthran@users.noreply.github.com>
2023-05-11 11:03:05 +0100

    extend console commands (#2)

--- console.py
+++ console.py
@@ -21,5 +21,27 @@ class HBNBCommand(cmd.Cmd):
         """Command to not execute any empty line entered"""
         pass
 
+    def do_create(self, class_name):
+        '''Creates a new class instance and saves it'''
+        pass
+
+    def do_show(self, class_name, class_id):
+        '''Prints an instance of an object
+        represented by class_name and class_id'''
+        pass
+
+    def do_destroy(self, class_name, class_id):
+        '''Deletes an instance and save changes'''
+        pass
+
+    def do_all(self, class_name):
+        '''Prints all instances in an object file'''
+        pass
+
+    def do_update(self, **kwargs):
+        '''Updates a class one attribute per time
+        and saves'''
+        pass
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
